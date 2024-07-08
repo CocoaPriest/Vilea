b846bcc06ac5e829bf81d8e35c596bb3d5e430c9
Konstantin Gonikman <info@alstertouch.com>
2024-07-08 22:30:46 +0200

    Initial Commit

---
+++ Vilea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vilea
+//
+//  Created by Konstantin Gonikman on 08.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
